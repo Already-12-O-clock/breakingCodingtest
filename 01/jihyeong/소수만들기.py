e6d71d0edc21878e98ab5401c0568aa11bf952f0
jihyunglee <jhlee7125@gmail.com>
2023-03-19 14:13:27 +0900

    answer

---
+++ 01/jihyeong/소수만들기.py
@@ -0,0 +1,14 @@
+def solution(nums):
+    from itertools import combinations as cb
+    answer = 0
+    for a in cb(nums, 3):
+        cand = sum(a)
+        for j in range(2, cand):
+            if cand%j==0:
+                break
+        else:
+            answer += 1
+    return answer
+
+print(solution([1, 2, 3, 4])) # 1
+print(solution([1, 2, 7, 6, 4])) # 4
